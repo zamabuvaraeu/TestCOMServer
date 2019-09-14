48d39154eb81dc1e61846efea89de83a2d24804e
Эрик Замабувараев‐Ёмолкуу <erik@freebasic.su>
2019-09-14 21:29:44 +0700

    CoClass

--- make.cmd
+++ make.cmd
@@ -15,36 +15,36 @@ set MaxErrorsCount=-maxerr 1
 set MinWarningLevel=-w all
 set OptimizationLevel=-O 3
 set VectorizationLevel=-vec 0
+REM set EnableShowIncludes=-showincludes
 REM set EnableVerbose=-v
 REM set EnableRuntimeErrorChecking=-e
 REM set EnableDebug=-g
 REM set EnableFunctionProfiling=-profile
-REM set EnableShowIncludes=-showincludes
 
 set MainFile=Modules\DLLMain.bas
-set Classes=
-set Modules=
+set Classes=Classes\TestCOMServer.bas Classes\TestCOMServerClassFactory.bas
+set Modules=Modules\InitializeVirtualTables.bas Modules\Registry.bas
 set Resources=Resources.rc
 
 if "%1"=="lib" (
 	set DYNAMICLIBRARY_DEFINED=
 	set Win32Subsystem=console
-	set OutputFileName=libTestCOMServer.a
+	set OutputFileName=libBatchedFilesTestCOMServer.a
 	set TypeKindLibrary=-lib
 ) else (
 	set DYNAMICLIBRARY_DEFINED=
 	set Win32Subsystem=console
-	set OutputFileName=TestCOMServer.dll
-	set OutputDefinitionFileName=TestCOMServer.def
+	set OutputFileName=BatchedFilesTestCOMServer.dll
+	set OutputDefinitionFileName=BatchedFilesTestCOMServer.def
 	set TypeKindLibrary=-dll
 )
 
 if "%2"=="withoutruntime" (
-	set WITHOUT_RUNTIME_DEFINED=-d withoutruntime
+	set WITHOUT_RUNTIME_DEFINED=-d WITHOUT_RUNTIME_DEFINED
 	set UseThreadSafeRuntime=
 	set WriteOutOnlyAsm=-r
 	set TypeKindLibrary=-lib
-	set IncludeObjectLibraries=-lkernel32 -lgdi32 -lmsimg32 -luser32 -lversion -ladvapi32 -limm32 -loleaut32 -lole32 -luuid -lpsapi -lshlwapi -lshell32 -lcomctl32 -lfbmt -lgcc -lmsvcrt -lmingw32 -lmingwex -lmoldname -lgcc_eh
+	set IncludeObjectLibraries=-lkernel32 -lgdi32 -lmsimg32 -luser32 -lversion -ladvapi32 -limm32 -luuid -loleaut32 -lole32 -lpsapi -lshlwapi -lshell32 -lcomctl32 -lmsvcrt
 	set GCCWarning=-Werror -Wall -Wno-unused-label -Wno-unused-function -Wno-unused-variable -Wno-unused-but-set-variable -Wno-main
 	set GCCNoInclude=-nostdlib -nostdinc
 	set GCCArchitecture=-m64 -march=x86-64
@@ -59,11 +59,11 @@ if "%2"=="withoutruntime" (
 "%CompilerDirectory%\fbc.exe" %MaxErrorsCount% %UseThreadSafeRuntime% -x %OutputFileName% %IncludeLibraries% %IncludeFilesPath% %OptimizationLevel% -s %Win32Subsystem% %VectorizationLevel% -gen %CodeGenerationBackend% %MinWarningLevel% %EnableDebug% %EnableFunctionProfiling% %WriteOutOnlyAsm% %TypeKindLibrary% %EnableShowIncludes% %EnableVerbose% %DYNAMICLIBRARY_DEFINED% %WITHOUT_RUNTIME_DEFINED% %MainFile% %Classes% %Modules% %Resources% >_out.txt
 
 if %errorlevel% GEQ 1 (
-	goto :EOF
+	exit /b 1
 )
 
 if not "%2"=="withoutruntime" (
-	goto :EOF
+	exit /b
 )
 
 set AllCompiledFiles=%MainFile% %Classes% %Modules%
@@ -83,19 +83,21 @@ if "%PROCESSOR_ARCHITECTURE%"=="x86" (
 ) else (
 	set EntryPoint=DllMain
 	set PEFileFormat=i386pep
+	set MajorImageVersion=--major-image-version 1
+	set MinorImageVersion=--minor-image-version 0
 )
 
 "%CompilerBinDirectory%\ld.exe" -m %PEFileFormat% -subsystem console --dll --enable-stdcall-fixup -e %EntryPoint% -s --stack 1048576,1048576 --output-def %OutputDefinitionFileName% -L "%CompilerLibDirectory%" -L "." %AllObjectFiles% -o %OutputFileName% -( %IncludeObjectLibraries% -)
-
+del %AllObjectFiles%
 "%CompilerBinDirectory%\dlltool.exe" --def %OutputDefinitionFileName% --dllname %OutputFileName% --output-lib lib%OutputFileName%.a
 
-goto :EOF
+exit /b
+
 
 :gcccompile
 
-set FileWithExtension=%1
-set FileWithoutExtension=%FileWithExtension:~0,-3%
-
+set FileWithExtensionBas=%1
+set FileWithoutExtension=%FileWithExtensionBas:~0,-3%
 set FileWithExtensionC=%FileWithoutExtension%c
 set FileWithExtensionAsm=%FileWithoutExtension%asm
 set FileWithExtensionObj=%FileWithoutExtension%obj
@@ -105,9 +107,11 @@ if "%PROCESSOR_ARCHITECTURE%"=="x86" (
 ) else (
 	set TargetAssemblerArch=--64
 	"%CompilerBinDirectory%\gcc.exe" %GCCWarning% %GCCNoInclude% %GCCOptimizations% %GCCArchitecture% -masm=intel -S %FileWithExtensionC% -o %FileWithExtensionAsm%
+	del %FileWithExtensionC%
 )
 
 "%CompilerBinDirectory%\as.exe" %TargetAssemblerArch% --strip-local-absolute %FileWithExtensionAsm% -o %FileWithExtensionObj%
+del %FileWithExtensionAsm%
 
 set AllObjectFiles=%AllObjectFiles% %FileWithExtensionObj%
 
