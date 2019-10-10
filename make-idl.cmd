f81b613e39b045ea5f39c1201bbfeb19ac11a6fd
Эрик Замабувараев‐Ёмолкуу <erik@freebasic.su>
2019-10-10 12:42:40 +0700

    Возвращение NULL при невозможности получить библиотеку типов

--- make-idl.cmd
+++ make-idl.cmd
@@ -1,1 +1,3 @@
-midl idl\BatchedFilesTestCOMServer.idl
+set MidlDirectory=C:\Program Files (x86)\Windows Kits\10\bin\10.0.15063.0\x86
+rem "%MidlDirectory%\midl.exe" idl\BatchedFilesTestCOMServer.idl
+midl.exe idl\BatchedFilesTestCOMServer.idl
