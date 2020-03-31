edafb3d393c68348f835e623dc7924673c80c077
Эрик Замабувараев‐Ёмолкуу <erik@freebasic.su>
2020-04-01 02:30:57 +0700

    Добавление функций родительского интерфейса

--- make-idl.cmd
+++
@@ -1,3 +0,0 @@
-set MidlDirectory=C:\Program Files (x86)\Windows Kits\10\bin\10.0.15063.0\x86
-rem "%MidlDirectory%\midl.exe" idl\BatchedFilesTestCOMServer.idl
-midl.exe idl\BatchedFilesTestCOMServer.idl
