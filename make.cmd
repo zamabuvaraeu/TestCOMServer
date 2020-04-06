985ed29c4fe5e25dcd57ec109fa907d33829e1ea
Эрик Замабувараев‐Ёмолкуу <erik@freebasic.su>
2020-04-07 01:40:36 +0700

    Точки подключения

--- make.cmd
+++ make.cmd
@@ -1,8 +1,8 @@
 set CompilerDirectory=%ProgramFiles%\FreeBASIC
 
 set MainFile=Modules\DLLMain.bas
-set Classes=Classes\ClassFactory.bas Classes\ObjectsCounter.bas Classes\TestCOMServer.bas
-set Modules=Modules\ClassFactoryVtbl.bas Modules\Guids.bas Modules\Registry.bas Modules\TestCOMServerVtbl.bas
+set Classes=Classes\ClassFactory.bas Classes\ObjectsCounter.bas Classes\TestCOMServer.bas Classes\TestCOMServerEventsConnectionPoint.bas
+set Modules=Modules\ClassFactoryVtbl.bas Modules\Guids.bas Modules\Registry.bas Modules\TestCOMServerVtbl.bas Modules\TestCOMServerEventsConnectionPointVtbl.bas
 set Resources=Resources.RC
 set OutputFile=BatchedFilesTestCOMServer.dll
 
