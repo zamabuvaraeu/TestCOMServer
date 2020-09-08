663416a3b7e303aa24cd82c8e040832e508c684b
Эрик Замабувараев‐Ёмолкуу <erik@freebasic.su>
2020-09-08 19:06:52 +0700

    Не создавать отдельные файлы для таблиц виртуальных функций.

--- make.cmd
+++ make.cmd
@@ -2,7 +2,7 @@ set CompilerDirectory=%ProgramFiles%\FreeBASIC
 
 set MainFile=Modules\DLLMain.bas
 set Classes=Classes\ClassFactory.bas Classes\ObjectsCounter.bas Classes\TestCOMServer.bas Classes\TestCOMServerEventsConnectionPoint.bas
-set Modules=Modules\ClassFactoryVtbl.bas Modules\Guids.bas Modules\InvokeDispatchFunction.bas Modules\Registry.bas Modules\TestCOMServerVtbl.bas Modules\TestCOMServerEventsConnectionPointVtbl.bas
+set Modules=Modules\Guids.bas Modules\InvokeDispatchFunction.bas Modules\Registry.bas
 set Resources=Resources.RC
 set OutputFile=BatchedFilesTestCOMServer.dll
 
